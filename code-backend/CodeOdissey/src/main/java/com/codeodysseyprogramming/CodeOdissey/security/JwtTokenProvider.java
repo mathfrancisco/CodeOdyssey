74c4d4cd936fd35c8e9187a3f41d6cb87f2bc73d
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-02-18 14:22:09 -0300

    Update JwtTokenProvider.java
    Criação Do JwtToken, Gera e valida tokens JWT

---
+++ code-backend/CodeOdissey/src/main/java/com/codeodysseyprogramming/CodeOdissey/security/JwtTokenProvider.java
@@ -0,0 +1,64 @@
+// JwtTokenProvider.java
+package com.codeodisseyprogramming.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt.expiration}")
+    private int jwtExpirationInMs;
+
+    public String generateToken(Authentication authentication) {
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+
+        return Jwts.builder()
+                .setSubject(userPrincipal.getId())
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException ex) {
+            // Log error
+            return false;
+        } catch (MalformedJwtException ex) {
+            // Log error
+            return false;
+        } catch (ExpiredJwtException ex) {
+            // Log error
+            return false;
+        } catch (UnsupportedJwtException ex) {
+            // Log error
+            return false;
+        } catch (IllegalArgumentException ex) {
+            // Log error
+            return false;
+        }
+    }
+}
