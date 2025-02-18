a93ef4ad81a6398977b5fbfcfbde8d2c57417a3c
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-02-18 14:54:12 -0300

    Update UserDetailsServiceImpl.java

---
+++ code-backend/CodeOdissey/src/main/java/com/codeodysseyprogramming/CodeOdissey/security/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.codeodysseyprogramming.CodeOdissey.security;
+
+import com.codeodysseyprogramming.CodeOdissey.models.User;
+import com.codeodysseyprogramming.CodeOdissey.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPasswordHash())
+                .authorities(Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().toUpperCase())))
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+}
