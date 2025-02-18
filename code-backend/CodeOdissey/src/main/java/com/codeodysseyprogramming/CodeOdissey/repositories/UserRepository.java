db450036b571481ba268522aea8e5279760d1285
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-02-18 14:20:27 -0300

    Update UserRepository.java

---
+++ code-backend/CodeOdissey/src/main/java/com/codeodysseyprogramming/CodeOdissey/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.codeodisseyprogramming.repositories;
+
+import com.codeodisseyprogramming.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
