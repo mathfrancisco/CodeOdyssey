5fb078bd47f4eda8a075291980b30cce029fecda
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-02-18 13:37:18 -0300

    ATT

---
+++ code-backend/CodeOdissey/src/main/java/com/codeodysseyprogramming/CodeOdissey/CodeOdisseyApplication.java
@@ -0,0 +1,13 @@
+package com.codeodysseyprogramming.CodeOdissey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeOdisseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeOdisseyApplication.class, args);
+	}
+
+}
