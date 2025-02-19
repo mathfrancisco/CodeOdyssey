632b3a7c5507976fa2c76d8fbdd680cea3e1cb35
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-02-19 14:26:44 -0300

    Update ExerciseRepository.java

--- code-backend/CodeOdissey/src/main/java/com/codeodysseyprogramming/CodeOdissey/repositories/ExerciseRepository.java
+++ code-backend/CodeOdissey/src/main/java/com/codeodysseyprogramming/CodeOdissey/repositories/ExerciseRepository.java
@@ -1,14 +1,7 @@
-package com.codeodysseyprogramming.CodeOdissey.repositories;
-
-
-import com.codeodysseyprogramming.CodeOdissey.models.Exercise;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import java.util.List;
-
 public interface ExerciseRepository extends MongoRepository<Exercise, String> {
     List<Exercise> findByLanguage(String language);
     List<Exercise> findByDifficulty(Exercise.Difficulty difficulty);
+    List<Exercise> findByCourseId(String courseId);
     
     @Query("{'language': ?0, 'difficulty': ?1}")
     List<Exercise> findByLanguageAndDifficulty(String language, Exercise.Difficulty difficulty);
