56be089443c87f6a03b13ade2d44549a6d3b8d4f
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-02-20 08:10:12 -0300

    Update Profile.tsx

--- code-frontend/src/pages/Profile.tsx
+++ code-frontend/src/pages/Profile.tsx
@@ -220,7 +220,6 @@ const Profile: React.FC = () => {
       </div>
     );
   }
-
   return (
     <div className="bg-gray-50 min-h-screen py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
