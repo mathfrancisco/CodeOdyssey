55c688370e08631a856a5757628eb813f92f820d
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-02-24 14:58:55 -0300

    Update auth.service.ts

--- code-frontend/src/services/auth.service.ts
+++ code-frontend/src/services/auth.service.ts
@@ -13,7 +13,7 @@ interface RegisterData {
 
 const authService = {
   async login(credentials: LoginCredentials) {
-    const response = await api.post('/auth/login', credentials);
+  const response = await api.post('/api/auth/login', credentials);
     if (response.data.token) {
       localStorage.setItem('token', response.data.token);
     }
